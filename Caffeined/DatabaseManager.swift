c2132efdec3120ed1bb2b06d8c8e6ff3848e6c3d
Stefan Rajkovic <stefanrajkovic@college.harvard.edu>
2015-01-06 23:33:59 -0500

    Basic implementation of a database manager

--- Caffeined/DatabaseManager.swift
+++ Caffeined/DatabaseManager.swift
@@ -8,4 +8,95 @@
 
 import Foundation
 
+/* Private Class Properites
+ * 
+ * These don't need to be visible to anyone else and are constant across all instances
+ * of this class, so they live up here.
+ *
+ */
+private let _defaultManager = DatabaseManager()
 
+private let documentsFolder = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+    .UserDomainMask, true)[0] as String
+
+private let databasePath = documentsFolder.stringByAppendingPathComponent("drinkDatabase.sqlite")
+
+private func copyDatabaseFile() {
+    if !NSFileManager.defaultManager().fileExistsAtPath(databasePath) {
+        NSFileManager.defaultManager().copyItemAtPath(NSBundle.mainBundle().pathForResource(
+            "drinkDatabase", ofType: "sqlite")!, toPath: databasePath, error: nil)
+    }
+}
+
+enum DrinkType: String {
+    case Tea = "tea"
+    case Coffee = "coffee"
+    case EnergyDrink = "energy"
+    case Soda = "soda"
+    case Other = "other"
+}
+
+class Drink : NSObject {
+    var name : String = ""
+    var caffeineContent : Double = 0
+    var commonSizes : Array<Int> = []
+    var type : DrinkType = DrinkType.Other
+    
+}
+class DatabaseManager : NSObject {
+    
+    private var drinkDatabase : FMDatabase?
+    
+    class func defaultManager() -> DatabaseManager {
+        return _defaultManager
+    }
+    
+    override init() {
+        super.init()
+        
+        copyDatabaseFile()
+    }
+    
+    private func initializeDatabase() {
+        
+        self.drinkDatabase = FMDatabase(path: databasePath)
+        
+        if !self.drinkDatabase!.open() {
+            NSLog("Shit, we couldn't open the database, better copy it back and try again")
+            self.drinkDatabase = nil;
+            
+            NSFileManager.defaultManager().removeItemAtPath(databasePath, error: nil)
+            copyDatabaseFile()
+            
+            self.drinkDatabase = FMDatabase(path: databasePath)
+            self.drinkDatabase!.open()
+            
+            return
+        }
+    }
+    // here we could have an else if we have multiple versions of this DB
+    // floating around, like if we added a column later but that's unnecessary right now
+    
+    func getDrinksForCategory(drinkTypeLookup : DrinkType) -> Array<Drink> {
+        
+        var drinks : Array<Drink> = []
+        
+        let results : FMResultSet = self.drinkDatabase!.executeQuery(
+            "SELECT * FROM drinks WHERE category = ?",
+            withArgumentsInArray: [drinkTypeLookup.rawValue])
+        
+        while results.next() {
+            let newDrink = Drink()
+            newDrink.name = results.stringForColumn("name")
+            newDrink.caffeineContent = results.doubleForColumn("caffeineContent")
+            // SQLite does not have support for arrays, so we split a comma separated string
+            newDrink.commonSizes = split(results.stringForColumn("sizes"),{$0 == ","},
+                allowEmptySlices:false).map({$0.toInt()!})
+            newDrink.type = DrinkType(rawValue: results.stringForColumn("type"))!
+            drinks.append(newDrink)
+        }
+        return drinks
+    }
+    
+    
+}
